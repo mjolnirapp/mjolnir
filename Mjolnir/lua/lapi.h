6907e7a21b9bb1c24cd1b568a7ecef673baf4f5f
Charles Gardner <charles@opsroll.com>
2019-05-07 15:27:06 -0700

    Updated static LUA to 5.3.5

--- Mjolnir/lua/lapi.h
+++ Mjolnir/lua/lapi.h
@@ -1,5 +1,5 @@
 /*
-** $Id: lapi.h,v 2.7.1.1 2013/04/12 18:48:47 roberto Exp $
+** $Id: lapi.h,v 2.9.1.1 2017/04/19 17:20:42 roberto Exp $
 ** Auxiliary functions from Lua API
 ** See Copyright Notice in lua.h
 */
